2e316086e3675f332452191afeeb92563d47ae2a
Bill Gao <Billsfriend1999@outlook.com>
2020-06-11 20:54:24 +0800

    try user login logic

--- CLO_main.py
+++ CLO_main.py
@@ -4,8 +4,23 @@
 import os
 
 # welcome page
-print('Welcome to Campusia Live Online!')
+print('Welcome to Campusia Live Online!') 
 
-usrid = input('Please enter your ID: ')
+try:
+    login = open(r'.\userdata.txt', 'r')
+    usr = login.readlines()
+    print('Welcome back,', usr[0].strip(), '!')
+    inp = input('please enter your password:')
+except IOError:
+    login = open(r'.\userdata.txt', 'x')# create a new file
+    inp = input('Hello, freshman! Please enter your ID to sign up:')
+    login.write(inp)
+    login.write('\n')
+    print('So your ID is', inp, '!\n')
+    pss = input('Please enter your password and remember it:')
+    login.write(pss)
+finally:
+    if login:
+        login.close()
 
-login = open(r'\userdata.txt', 'x') # create a new file
+print('Here is the end of DEMO.')
